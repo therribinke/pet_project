c67f798675b53725314d701a3d7eb3fd9c72bc34
therribinke <thekuznetsoff@yandex.ru>
2023-10-11 19:19:12 +0300

    Version 3

--- calculator/src/Main.java
+++ calculator/src/Main.java
@@ -3,27 +3,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Введите первое число");
-        float num1 = input.nextFloat();
-        System.out.println("Введите второе число");
-        float num2 = input.nextFloat();
-        System.out.println("Выберите операцию :\n '+' - сложение \n '-' - вычитание \n '*' - умножение \n '/' - деление \n");
-        String operation = input.next();
-        float result=0;
-        switch (operation){
-            case ("+"):
-                result=num1+num2;
-                break;
-            case ("-"):
-                result=num1-num2;
-                break;
-            case ("*"):
-                result=num1*num2;
-                break;
-            case ("/"):
-                result=num1/num2;
-                break;
+        System.out.println("Введите  число");
+        float result = input.nextFloat();
+        while (true) {
+            System.out.println("Выберите операцию :\n '+' - сложение \n '-' - вычитание \n '*' - умножение \n '/' - деление \n");
+            String operation = input.next();
+            System.out.println("Введите  число");
+            float num = input.nextFloat();
+            switch (operation) {
+                case ("+"):
+                    result += num;
+                    break;
+                case ("-"):
+                    result -= num;
+                    break;
+                case ("*"):
+                    result *= num;
+                    break;
+                case ("/"):
+                    result /= num;
+                    break;
+            }
+            System.out.println("Результат = " + result);
         }
-        System.out.println("Результат = "+ result);
     }
 }
