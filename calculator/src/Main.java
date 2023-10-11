bea51b3146465a7a31468c5d0504b2b1abe3d41a
therribinke <thekuznetsoff@yandex.ru>
2023-10-11 19:07:59 +0300

    Version 2

--- calculator/src/Main.java
+++ calculator/src/Main.java
@@ -1,7 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        float a = 4;
-        float b = 5;
-        System.out.println(a*b);
+        Scanner input = new Scanner(System.in);
+        System.out.println("Введите первое число");
+        float num1 = input.nextFloat();
+        System.out.println("Введите второе число");
+        float num2 = input.nextFloat();
+        System.out.println("Выберите операцию :\n '+' - сложение \n '-' - вычитание \n '*' - умножение \n '/' - деление \n");
+        String operation = input.next();
+        float result=0;
+        switch (operation){
+            case ("+"):
+                result=num1+num2;
+                break;
+            case ("-"):
+                result=num1-num2;
+                break;
+            case ("*"):
+                result=num1*num2;
+                break;
+            case ("/"):
+                result=num1/num2;
+                break;
+        }
+        System.out.println("Результат = "+ result);
     }
 }
